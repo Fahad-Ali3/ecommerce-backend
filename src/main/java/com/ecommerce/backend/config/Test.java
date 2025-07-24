6cd3a9f1e164a29aa772005267baf876044d0af4
Fahad-Ali3 <146737179+fahad-ali3@users.noreply.github.com>
2025-07-24 21:36:16 +0500

    testing..

--- src/main/java/com/ecommerce/backend/config/Test.java
+++ src/main/java/com/ecommerce/backend/config/Test.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class Test {
     public static void main(String[] args) {
         BCryptPasswordEncoder cryptPasswordEncoder=new BCryptPasswordEncoder();
-        System.out.println(cryptPasswordEncoder.encode("khan1234"));
-        System.out.println(cryptPasswordEncoder.encode("Saeed124@"));
+        System.out.println(cryptPasswordEncoder.encode("testing"));
+        System.out.println(cryptPasswordEncoder.encode("4@"));
     }
 }
